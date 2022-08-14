053281b5599175a2a97f7ef9a9dca4e0aeeccde2
jia8769 <76929813+jia8769@users.noreply.github.com>
2022-08-14 08:15:52 +0100

    set up

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,59 @@
-import React from "react";
-import HeadComponent from '../components/Head';
+import React, { useEffect, useState } from 'react';
+import Product from "../components/Product";
+
+import { useWallet } from '@solana/wallet-adapter-react';
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 
 // Constants
-const TWITTER_HANDLE = "_buildspace";
+const TWITTER_HANDLE = "jia_ayo";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
-  
+  const { publicKey } = useWallet();
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    if (publicKey) {
+      fetch(`/api/fetchProducts`)
+        .then(response => response.json())
+        .then(data => {
+          setProducts(data);
+          console.log("Products", data);
+        });
+    }
+  }, [publicKey]);
+
+  const renderNotConnectedContainer = () => (
+    <div>
+      <img src="money-exchange.png" alt="emoji" width={360} height={360} />
+
+      <div className="button-container">
+        <WalletMultiButton className="cta-button connect-wallet-button" />
+      </div>    
+    </div>
+  );
+
+  const renderItemBuyContainer = () => (
+    <div className="products-container">
+      {products.map((product) => (
+        <Product key={product.id} product={product} />
+      ))}
+    </div>
+  );
+
   
   return (
     <div className="App">
-      <HeadComponent/>
       <div className="container">
         <header className="header-container">
-          <p className="header"> 😳 Buildspace Emoji Store 😈</p>
-          <p className="sub-text">The only emoji store that accepts sh*tcoins</p>
+          <p className="header">Jia  Store  🛒</p>
+          <p className="sub-text">The only nigerian store that allows you to pay in solana <img src="solana.png" width={50} height={50}/> </p>
         </header>
 
         <main>
-          <img src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif" alt="emoji" />
+          {/* We only render the connect button if public key doesn't exist */}
+          {publicKey ? 'Connected!' : renderNotConnectedContainer()}
+
         </main>
 
         <div className="footer-container">
@@ -28,7 +63,7 @@ const App = () => {
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          >{`built by @${TWITTER_HANDLE}`}</a>
         </div>
       </div>
     </div>
